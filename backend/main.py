9d3097c01ecec9d5f25defd4f0893247bf2ce773
hellomyzn <em711th@icloud.com>
2022-07-04 08:23:59 +0900

    add on csv

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 # import controllers.conversation
 import os
+import datetime
 import spotipy
+import csv
 
 def setting():
     # get api data from environment environment variables
@@ -15,7 +17,7 @@ def get_tracks_from_playlist(playlist_id: int):
     playlist = spotify.playlist(playlist_id)
     return playlist["tracks"]["items"]
 
-def sort_data(tracks):
+def sort_data(tracks) -> list:
     
     names =         [ t["track"]["name"] for t in tracks]
     urls =          [ t["track"]["external_urls"]["spotify"] for t in tracks]
@@ -31,10 +33,39 @@ def sort_data(tracks):
                         'url':          urls[i],
                         'artist':       artists[i],
                         'release_date': release_date[i],
-                        'added_at':     added_at[i]}
+                        'added_at':     added_at[i],
+                        'created_at':   datetime.datetime.now()}
                         )
-    for i in range(num_tracks):
-        print(tracks[i] , "\n")
+    return tracks
+
+def create_columns_csv(url, columns):
+    
+    print('Create header on CSV')
+    # TODO: Get this url from config.ini
+    with open(url, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        writer.writeheader()
+
+def is_not_columns(url):
+    with open(url, 'r', newline='') as csvfile:
+        data = csvfile.readline()
+        if not data:
+            return True
+        else:
+            return False
+
+def write_csv(tracks, url, columns):
+    if is_not_columns(url):
+        create_columns_csv(url, columns)
+
+    with open(url, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        for track in tracks:
+            writer.writerow(track)
+            print(f"[WRITING]: {track}")
+
+
+
 
 
 
@@ -42,9 +73,12 @@ def sort_data(tracks):
     # controller.conversation.talk_about_input_vocabulary()
 
 if __name__ == "__main__":
+    columns = ['name', 'artist', 'url', 'release_date', 'added_at', 'created_at']
+    csv_url = './src/csv/tracks.csv'
     spotify = setting()
     playlist_id_ph = "37i9dQZEVXbNBz9cRCSFkY"
     tracks_ph = get_tracks_from_playlist(playlist_id_ph)
-    sort_data(tracks_ph)
+    tracks_ph = sort_data(tracks_ph)
+    write_csv(tracks_ph, csv_url, columns)
 
     # main()
