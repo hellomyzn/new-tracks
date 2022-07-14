b73bcb34b359da17621989f522540580a0f41141
hellomyzn <em711th@icloud.com>
2022-07-14 17:00:59 +0900

    refactor all with pycodestyle

--- backend/helper.py
+++ backend/helper.py
@@ -2,32 +2,36 @@ import os
 import pathlib
 import datetime
 
+
 def is_file(path: str):
     if os.path.isfile(path):
         return True
     return False
 
+
 def create_file(path: str):
     pathlib.Path(path).touch()
 
+
 def delete_file(path: str):
     os.remove(path)
+
 
 def is_yes(user_input):
     if user_input.lower() == 'y' or user_input.lower() == 'yes':
         return True
     return False
 
+
 def is_no(user_input):
     if user_input.lower() == 'n' or user_input.lower() == 'no':
         return True
     return False
 
+
 def get_date():
     t_delta = datetime.timedelta(hours=9)
     JST = datetime.timezone(t_delta, 'JST')
     now = datetime.datetime.now(JST)
-    date = now.strftime('%Y/%m/%d') 
-
+    date = now.strftime('%Y/%m/%d')
     return date
-        
--- backend/setting.py
+++ backend/setting.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 
+
 # Set up config as dict
 config_file = 'config.ini'
 CONFIG = ConfigParser()
@@ -15,19 +16,18 @@ FILE_PATH_OF_CSV = CONFIG['FILES']['DIR_CSV'] + CONFIG['FILES']['FILENAME_OF_CSV
 MY_PLAYLIST_ID = CONFIG['PLAYLIST_ID']['MY_PLAYLIST']
 
 
-PLAYLISTS_IDS = [
-    "37i9dQZEVXbMDoHDwVN2tF", # GLOBAL
-    "37i9dQZEVXbLRQDuF5jeBp", # US
-    "37i9dQZEVXbKXQ4mDTEBXq", # JP
-    "37i9dQZEVXbNBz9cRCSFkY", # PH
-    "37i9dQZEVXbLZ52XmnySJg", # IN
-    "37i9dQZEVXbLnolsZ8PSNw", # UK
-    "37i9dQZEVXbNxXF4SkHj9F", # KR
-    "37i9dQZEVXbNFJfN1Vw8d9", # SP
-    "37i9dQZEVXbJPcfkRz0wJ0", # AU
-    "37i9dQZEVXbJiZcmkrIHGU", # GE
-    "37i9dQZEVXbIQnj7RRhdSX", # IT
-    "37i9dQZEVXbIPWwFssbupI", # FR
-    "37i9dQZEVXbMXbN3EUUhlg", # BR
-    "37i9dQZF1DX4JAvHpjipBk"  # New Music Friday
-]
+PLAYLISTS_IDS = ["37i9dQZEVXbMDoHDwVN2tF",  # GLOBAL
+                 "37i9dQZEVXbLRQDuF5jeBp",  # US
+                 "37i9dQZEVXbKXQ4mDTEBXq",  # JP
+                 "37i9dQZEVXbNBz9cRCSFkY",  # PH
+                 "37i9dQZEVXbLZ52XmnySJg",  # IN
+                 "37i9dQZEVXbLnolsZ8PSNw",  # UK
+                 "37i9dQZEVXbNxXF4SkHj9F",  # KR
+                 "37i9dQZEVXbNFJfN1Vw8d9",  # SP
+                 "37i9dQZEVXbJPcfkRz0wJ0",  # AU
+                 "37i9dQZEVXbJiZcmkrIHGU",  # GE
+                 "37i9dQZEVXbIQnj7RRhdSX",  # IT
+                 "37i9dQZEVXbIPWwFssbupI",  # FR
+                 "37i9dQZEVXbMXbN3EUUhlg",  # BR
+                 "37i9dQZF1DX4JAvHpjipBk"   # New Music Friday
+                 ]
