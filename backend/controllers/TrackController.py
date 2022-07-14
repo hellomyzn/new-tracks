113975aad5265de2129fa5d2069286f347339298
hellomyzn <em711th@icloud.com>
2022-07-14 16:17:24 +0900

    refactore with pycodestyle

--- backend/controllers/TrackController.py
+++ backend/controllers/TrackController.py
@@ -17,26 +17,29 @@ class TrackController(object):
 
     def show_current_track_from_csv(self) -> None:
         track = SpotifyService.get_current_track(self.spotify)
-        if track: 
+        if track:
             track = CsvService.get_track_by_name_and_artist(self.csv.file_path,
                                                             track[0]['name'],
                                                             track[0]['artist'])
-            print(f'\n\tname:     {track[0]} \n\tartist:   {track[1]} \n\tplaylist: {track[2]} \n')
+            print(f'\n\tname:     {track[0]} \
+                    \n\tartist:   {track[1]} \
+                    \n\tplaylist: {track[2]} \n')
         else:
-            print("[WARNING] - There is no track you are currently listening on CSV")
-        
+            print("There is no current track")
         return
-
 
     def add_new_tracks_to_playlist(self) -> None:
         all_new_tracks = []
         # Add new tracks by each playlist
         for playlist_id in setting.PLAYLISTS_IDS:
             # Retrieve tracks data from playlist
-            tracks = SpotifyService.retrieve_tracks_from_playlist(self.spotify, playlist_id)
+            tracks = SpotifyService.retrieve_tracks_from_playlist(self.spotify,
+                                                                  playlist_id)
 
             # Retrieve only new tracks
-            new_tracks = SpotifyService.retrieve_new_tracks(self.spotify, tracks,  self.csv.file_path)
+            new_tracks = SpotifyService.retrieve_new_tracks(self.spotify,
+                                                            tracks,
+                                                            self.csv.file_path)
 
             # Add tracks to CSV
             CsvService.add_tracks(self.csv, new_tracks)
@@ -46,20 +49,26 @@ class TrackController(object):
                 all_new_tracks += new_tracks
 
         # Add tracks to google spreadsheet
-        GoogleSpreadsheetService.add_tracks(self.google_spreadsheet, all_new_tracks)
+        GoogleSpreadsheetService.add_tracks(self.google_spreadsheet,
+                                            all_new_tracks)
 
         # Add tracks to a playlist on Spotify
-        SpotifyService.add_tracks_to_playlist(self.spotify, all_new_tracks, setting.MY_PLAYLIST_ID)
+        SpotifyService.add_tracks_to_playlist(self.spotify,
+                                              all_new_tracks,
+                                              setting.MY_PLAYLIST_ID)
 
         return
 
     def remove_tracks_from_playlist(self) -> None:
-        if helper.is_yes(input("\nDo you want to remove some tracks you've already listened from playlist? (y/n): ")):
-            SpotifyService.remove_tracks_played_recently_from_playlist(self.spotify, setting.MY_PLAYLIST_ID)
+        question = "\nDo you want to remove some tracks you've already listened from playlist? (y/n): "
+        if helper.is_yes(input(question)):
+            SpotifyService.remove_tracks_played_recently_from_playlist(self.spotify,
+                                                                       setting.MY_PLAYLIST_ID)
         else:
             print('You can remove tracks between the track number(first) you choose and the track number(last) you choose')
             first = int(input('Enter a track number (first): '))
             last = int(input('Enter a track number (last): '))
-            SpotifyService.remove_tracks_from_playlist(self.spotify, setting.MY_PLAYLIST_ID, first, last)
-
+            SpotifyService.remove_tracks_from_playlist(self.spotify,
+                                                       setting.MY_PLAYLIST_ID, first,
+                                                       last)
         return
