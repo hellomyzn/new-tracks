08d8f074f3e6c365e7f76a408baba955e695b571
hellomyzn <em711th@icloud.com>
2022-08-02 17:52:44 +0900

    remove some unnecesary files

--- backend/models/csv.py
+++
@@ -1,9 +0,0 @@
-import utils.setting as setting
-from models.interfaces.table import Table
-
-
-class Csv(Table):
-    def __init__(self):
-        self.columns = Csv.get_columns()
-        self.path = setting.FILE_PATH_OF_CSV
-        self.dir_path = setting.DIR_PATH_OF_CSV
